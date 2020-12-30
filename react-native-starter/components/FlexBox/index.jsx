b2aca064aa3d54fe79d81dca9280002249886516
rexsimiloluwah <adetoyosi2001@gmail.com>
2020-12-30 01:24:42 +0000

    Learnt some Flexbox

--- react-native-starter/components/FlexBox/index.jsx
+++ react-native-starter/components/FlexBox/index.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
-import {View, Text. StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default function FlexBox(){
     return(
-
+        <View style = {styles.flexContainer}>
+            <Text style = {styles.One}>One</Text>
+            <Text style = {styles.Two}>Two</Text>
+            <Text style = {styles.Three}>Three</Text>
+            <Text style = {styles.Four}>Four</Text>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    flexContainer : {
+        // flex: 1,
+        backgroundColor: '#eee',
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+
+    One : {
+        flex: 2,
+        backgroundColor: 'crimson',
+        padding: 10
+    },
+
+    Two : {
+        flex: 1,
+        backgroundColor : 'coral',
+        padding: 10
+    },
+
+    Three : {
+        flex: 1,
+        backgroundColor: 'dodgerblue',
+        padding: 10
+    },
+
+    Four : {
+        flex: 2,
+        backgroundColor: 'rebeccapurple',
+        padding: 10
+    }
 })
