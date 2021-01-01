8a5a44abb47e9f76cdf07fd2f38eae56d3e5cd52
rexsimiloluwah <adetoyosi2001@gmail.com>
2021-01-01 22:40:45 +0000

    Added full app

--- ideate/App.js
+++ ideate/App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, LogBox} from 'react-native';
+import Home from './screens/Home';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import {globalStyles} from "./styles/globalStyles";
+import Navigator from "./navigators/rootDrawer";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  else{
+    return(
+      <Navigator />
+    )
+  }
+  
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
---
+++ ideate/styles/globalStyles.js
@@ -0,0 +1,29 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+
+    container : {
+        flex: 1,
+        padding: 24
+    },
+
+    paragraph : {
+        marginVertical : 5,
+        lineHeight: 20
+    },
+
+    boldText : {
+        fontSize : 20,
+        fontWeight: 'bold'
+    },
+
+    smallText : {
+        fontSize : 10
+    },
+
+    panel : {
+        backgroundColor : '#eee',
+        padding: 10,
+        marginVertical : 12
+    }
+})
